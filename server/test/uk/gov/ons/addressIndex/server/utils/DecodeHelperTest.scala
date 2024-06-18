bb3024cf7e5dd0d2ef6c01569b97bd63383c0a2c
Richard Smith <richard.m.smith@ons.gov.uk>
2024-06-18 09:11:26 +0100

    add tests

--- server/test/uk/gov/ons/addressIndex/server/utils/DecodeHelperTest.scala
+++ server/test/uk/gov/ons/addressIndex/server/utils/DecodeHelperTest.scala
@@ -41,4 +41,26 @@ class DecodeHelperTest extends AnyFlatSpec with should.Matchers {
     // Then
     actual shouldBe expected
   }
+
+  it should "decode a URL containing a percentage sign" in {
+    // Given
+    val inputURL = "Big 10% Media"
+    val expected = "Big 10% Media"
+    // When
+    val actual = DecodeHelper.decodeUrl(inputURL)
+
+    // Then
+    actual shouldBe expected
+  }
+
+  it should "decode a URL containing a percentage sign and a an encoded value" in {
+    // Given
+    val inputURL = "%2526Big%2520Wave%2520Med%ia"
+    val expected = "&Big Wave Med%ia"
+    // When
+    val actual = DecodeHelper.decodeUrl(inputURL)
+
+    // Then
+    actual shouldBe expected
+  }
 }
